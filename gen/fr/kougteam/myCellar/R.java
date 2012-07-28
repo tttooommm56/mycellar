bec466c550115a49f9867a9d189a8711b35227d6
Thomas Cousin <tttooommm56@gmail.com>
2012-07-28 20:17:57 +0200

    Ebauche menu accueil

--- gen/fr/kougteam/myCellar/R.java
+++ gen/fr/kougteam/myCellar/R.java
@@ -18,12 +18,18 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_menu_add=0x7f020002;
+        public static final int ic_menu_view=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int mainItemImg=0x7f080001;
+        public static final int mainItemTitre=0x7f080002;
+        public static final int mainListView=0x7f080000;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main_item=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/fr/kougteam/myCellar/activity/MainActivity.java
+++ src/fr/kougteam/myCellar/activity/MainActivity.java
@@ -1,27 +1,73 @@
 package fr.kougteam.myCellar.activity;
 
-import fr.kougteam.myCellar.R;
-import fr.kougteam.myCellar.R.layout;
-import fr.kougteam.myCellar.R.menu;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import fr.kougteam.myCellar.R;
 
 public class MainActivity extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    
-}
+	 
+		private ListView menuListView;
+	 
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	 
+	        //Récupération de la listview créée dans le fichier main.xml
+	        menuListView = (ListView) findViewById(R.id.mainListView);
+	 
+	        //Création de la ArrayList qui nous permettra de remplire la listView
+	        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+	 
+	        HashMap<String, String> map = new HashMap<String, String>();
+	        map.put("titre", "Ajouter des bouteilles");
+	        map.put("img", String.valueOf(R.drawable.ic_menu_add));
+	        listItem.add(map);
+	 
+	        map = new HashMap<String, String>();
+	        map.put("titre", "Voir la cave");
+	        map.put("img", String.valueOf(R.drawable.ic_menu_view));
+	        listItem.add(map);
+	 
+	        //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
+	        SimpleAdapter menuAdapter = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.main_item,
+	               new String[] {"img", "titre"}, new int[] {R.id.mainItemImg, R.id.mainItemTitre});
+	 
+	        //On attribut à notre listView l'adapter que l'on vient de créer
+	        menuListView.setAdapter(menuAdapter);
+	 
+	        //Enfin on met un écouteur d'évènement sur notre listView
+	        menuListView.setOnItemClickListener(new OnItemClickListener() {
+				
+	        	@SuppressWarnings("unchecked")
+	         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+					//on récupère la HashMap contenant les infos de notre item (titre, description, img)
+	        		HashMap<String, String> map = (HashMap<String, String>) menuListView.getItemAtPosition(position);
+	        		//on créer une boite de dialogue
+	        		AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+	        		//on attribut un titre à notre boite de dialogue
+	        		adb.setTitle("Sélection Item");
+	        		//on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
+	        		adb.setMessage("Votre choix : "+map.get("titre"));
+	        		//on indique que l'on veut le bouton ok à notre boite de dialogue
+	        		adb.setPositiveButton("Ok", null);
+	        		//on affiche la boite de dialogue
+	        		adb.show();
+	        	}
+	         });
+	 
+	    }
+	}
