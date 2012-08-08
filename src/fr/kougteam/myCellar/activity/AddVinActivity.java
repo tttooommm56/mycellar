53a671cf2731fd05236cbd27e9a10b00fc44fd4a
Sharp <baptiste.jeangeorges@gmail.com>
2012-08-08 20:15:54 +0200

    Premier commit de Baptiste

--- src/fr/kougteam/myCellar/activity/AddVinActivity.java
+++ src/fr/kougteam/myCellar/activity/AddVinActivity.java
@@ -11,6 +11,6 @@ public class AddVinActivity extends Activity {
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.add_vin);
+		setContentView(R.layout.add_vin); 
 	}
 }
